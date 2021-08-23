86fdbba33956619fb3e45c2b342e6324a5413a7c
Nastya <84792862+NastyaVl-droid@users.noreply.github.com>
2021-08-23 15:26:30 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 $(function() {
     var currentFloor = 2; //текущий этаж
-    var floorPath = $('.main__image-home path');
-    var counterUp = $('.counter-button_up'); //кнопка увеличения этажа
-    var counterDown = $('.counter-button_down'); // кнопка уменьшения этажа
+    var floorPath = $('.home-item'); // векторный этаж на картинке
+    var counterUp = $('.counter-button-up'); //кнопка увеличения этажа
+    var counterDown = $('.counter-button-down'); // кнопка уменьшения этажа
+    var modal = $('.modal'); // затемненная область 
+    var modalCounter = $('.modal-counter'); // счетчик этажей в диалоговом окне
+    var modalCloseButton = $('.modal-close-button'); 
+    var viewFlatsButton = $('.view-button');
+    var currentFlat = 40; // текущая квартира
+    var flatsPath = $('.current-flat');
+    var flatsLink = $('.flat-link');
+    
+    //функция при наведении курсора на ссылку
+    $(flatsPath).on('mouseover', function() {
+        currentFlat = $(this).attr('data-flat'); // получаем значение текущей квартиры с картинки
+        flatsPath.removeClass('current-flat'); // убираем активный класс текущего этажа
+        flatsLink.removeClass('current-flat-item');
+        $(`[data-flat = ${currentFlat}]`).toggleClass('current-flat'); // текущая квартира подсвечивается 
+        $(`[data-item = ${currentFlat}]`).toggleClass('current-flat-item'); // текущая ссылка подчеркивается и меняет цвет
+    });
+    
+    $(flatsLink).on('click', function() {
+        currentFlat = $(this).attr('data-item'); // получаем характеристики текущей ссылки на квартиру
+        flatsPath.removeClass('current-flat'); // убираем активный класс текущего этажа
+        flatsLink.removeClass('current-flat-item');
+        $(`[data-flat = ${currentFlat}]`).toggleClass('current-flat'); // текущая квартира подсвечивается 
+        $(`[data-item = ${currentFlat}]`).toggleClass('current-flat-item'); // текущая ссылка подчеркивается и меняет цвет   
+    });
     
     // функция при наведении курсора на этаж
     $(floorPath).on('mouseover', function() {
-        $(floorPath).removeClass('main__image-home-current-floor'); //удаляем активный класс у этажей
+        $(floorPath).removeClass('image-home-current-floor'); //удаляем активный класс у этажей
         currentFloor = $(this).attr('data-floor'); // получаем значение текущего этажа
-        $('.counter').text(currentFloor); 
+        $('.counter').text(currentFloor); // выводим в счетчик цифру текущего этажа
     });
     
     //функция отслеживания клика по кнопке Вверх
@@ -17,22 +41,36 @@ $(function() {
         currentFloor++;
         usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2,
         useGroupping: false });
-      $('.counter').text(usCurrentFloor);
-      $(floorPath).removeClass('main__image-home-current-floor');
-      $(`[data-floor = ${usCurrentFloor}]`).toggleClass('main__image-home-current-floor');
+      $('.counter').text(usCurrentFloor); // на счетчике выводится цифра текущего этажа
+      $(floorPath).removeClass('image-home-current-floor'); // удаляем активный класс текущего этажа
+      $(`[data-floor = ${usCurrentFloor}]`).toggleClass('image-home-current-floor'); 
+      // извлекаем номер этажа и присваиваем ему класс текущего этажа
   }
-    // функция отслеживания клика по кнопке Вниз
     });
+    
+    // функция отслеживания клика по кнопке Вниз
     counterDown.on('click', function() {
         if (currentFloor > 2) {
             currentFloor--;
             usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2,
-            useGroupping: false });
-        $('.counter').text(usCurrentFloor);
-        $(floorPath).removeClass('main__image-home-current-floor');
-        $(`[data-floor = ${usCurrentFloor}]`).toggleClass('main__image-home-current-floor');
+        useGroupping: false });
+        $('.counter').text(usCurrentFloor); // на счетчике выводится цифра текущего этажа
+      $(floorPath).removeClass('image-home-current-floor'); // удаляем активный класс текущего этажа
+      $(`[data-floor = ${usCurrentFloor}]`).toggleClass('image-home-current-floor'); 
+      // извлекаем номер этажа и присваиваем ему класс текущего этажа
         }
-    })
+    });
+    
+    floorPath.on('click', toggleModal); // при нажатии на этаж открывается модальное окно
+    modalCloseButton.on('click', toggleModal); // при нажатии на крестик закрывается модальное окно
+    viewFlatsButton.on('click', toggleModal); 
+    // при нажатии на кнопку открывается модальное окно
+    
+     // функция переключения класса для открытия модального окна 
+    function toggleModal() {
+        modal.toggleClass('is-open');
+        $(modalCounter).text(usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGroupping: false })); // в модальном окне меняется номер этажа
+    }
+   
 });
-
 
